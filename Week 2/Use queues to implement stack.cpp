823c3db7027e00e5d4dac9d92de7e7cc6812282b
minhuet <109837037+minh-1129@users.noreply.github.com>
2023-09-16 20:34:19 +0700

    fix

--- Week 2/Use queues to implement stack.cpp
+++ Week 2/Use queues to implement stack.cpp
@@ -1,39 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-template<class data>
-class Stack{
+template <class data>
+class Stack
+{
 private:
     queue<data> q1;
     queue<data> q2;
+
 public:
-    void push(data x){
-        while(!q1.empty()){
+    void push(data x)
+    {
+        while (!q1.empty())
+        {
             q2.push(q1.front());
             q1.pop();
         }
         q1.push(x);
-        while(!q2.empty()){
+        while (!q2.empty())
+        {
             q1.push(q2.front());
             q2.pop();
         }
     }
-    void pop(){
+    void pop()
+    {
+        if (q1.empty())
+        {
+            cout << "Error: empty stack\n";
+            exit(EXIT_FAILURE);
+        }
         q1.pop();
     }
-    data top(){
+    data top()
+    {
+        if (q1.empty())
+        {
+            cout << "Error: empty stack\n";
+            exit(EXIT_FAILURE);
+        }
         return q1.front();
     }
-    bool isEmpty(){
+    bool isEmpty()
+    {
         return q1.empty();
     }
 };
-int main(){
+int main()
+{
     Stack<int> st;
-    for(int i = 1; i <= 10; i++){
+    for (int i = 1; i <= 10; i++)
+    {
         st.push(i);
     }
-    while(!st.isEmpty()){
-        cout<<st.top()<<endl;
+    while (!st.isEmpty())
+    {
+        cout << st.top() << endl;
         st.pop();
     }
+    st.pop();
+    return 0;
 }
